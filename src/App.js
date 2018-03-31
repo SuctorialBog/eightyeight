a4ddbb8f1c54c809ce5c4198ade2c9d63e69472c
Skylar Everett <skylareverett86@gmail.com>
2018-03-30 22:12:33 -0500

    balling out of control with the completed styling.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './img/logo.svg';
 import './App.css';
+import './App.scss';
 import 'typeface-open-sans';
 import 'typeface-montserrat';
 import fontawesome from '@fortawesome/fontawesome';
@@ -27,19 +28,20 @@ class App extends Component {
     return (
       <div className="main">
         <img src={logo} className="ee-logo" alt="eightyeight logo" />
-        <h1>Product Designer,<br/>Front End Developer,<br />Husband & Father… of twins.</h1>
-        <p>My name is Skylar Everett. I have been helping people design and develop brands,
+        <h1 className="landing-head"><span>Product Designer,<br/>Front End Developer,</span><br />Husband & Father… of twins.</h1>
+        <hr className="landing-dash" />
+        <p className="landing-copy">My name is Skylar Everett. I have been helping people design and develop brands,
         websites, illustrations, and apps for the last eight years. If you’re looking to get
         something cooking, I’m always hungry. Simply give me a shout and we can talk about
         what’s on your mind. You can click the links below to download a copy of my resume,
         email me, check out my work on dribble, see my linkedin profile, or even take a look
         at my musical inclinations. I look forward to working with you!</p>
         <div className="home-links">
-          <a href="{process.env.PUBLIC_URL + '/skylar_everett_resume_2018.pdf'}">Resume</a>
-          <a href="mailto:hello@eighty-eight.co"><FontAwesomeIcon icon={faEnvelope} /></a>
-          <a href="https://dribbble.com/skylareverett"><FontAwesomeIcon icon={dribbble} /></a>
-          <a href="https://www.linkedin.com/in/skylareverett"><FontAwesomeIcon icon={linkedIn} /></a>
-          <a href="https://open.spotify.com/user/skytown?si=_484CT0ESu2BSbVAZSWx6A"><FontAwesomeIcon icon={spotify} /></a>
+          <a href="https://drive.google.com/file/d/14WiN4aDJvUxiaV3V7ecg6S96TBA-tk89/view?usp=sharing">Resume</a>
+          <a className="dots" href="mailto:hello@eighty-eight.co"><FontAwesomeIcon icon={faEnvelope} /></a>
+          <a className="dots" href="https://dribbble.com/skylareverett"><FontAwesomeIcon icon={dribbble} /></a>
+          <a className="dots" href="https://www.linkedin.com/in/skylareverett"><FontAwesomeIcon icon={linkedIn} /></a>
+          <a className="dots" href="https://open.spotify.com/user/skytown?si=_484CT0ESu2BSbVAZSWx6A"><FontAwesomeIcon icon={spotify} /></a>
         </div>
       </div>
     );
